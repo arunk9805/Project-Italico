9728bcf09157b4c370febbff1e493c94e19811b9
arunk9805 <arunk9805@gmail.com>
2024-03-31 18:21:26 +0530

    1.regSuccess.ejs updated. Registraion successful message page completed. Not resposive yet. Fetching data from databse and showing it on screen.

--- server.js
+++ server.js
@@ -3,32 +3,67 @@ import ejs from "ejs";
 import express from 'express';
 import bodyParser from "body-parser";
 import cors from 'cors';
-import GuestData from "./guest.model.js";
 import { dirname } from 'path';
 import { fileURLToPath } from "url";
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+//import routes and models
+import GuestData from "./guest.model.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
-app.use(express.static("public"));
 
+// Server static files
+app.use(express.static("public"));
+app.use("/CSS", express.static(__dirname + ".public/CSS"))
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + 'index.html');
+});
+
+function formatDate(Stringdate) {
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const date = new Date(Stringdate);
+    const dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay()];
+    const dateOfMonth = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    return [dayOfWeek, dateOfMonth, month, year];
+}
+
+// const temp ="";
+// const convertedDate =  formatDate("2024-03-31T00:00:00.000Z");
+// console.log(`${convertedDate[0]}, ${convertedDate[1]}, ${convertedDate[2]}, ${convertedDate[3]}`);
+// console.log(convertedDate);
+
+
+app.get("/submit", (req, res) => {
+    const saveGuestData = {
+        Name: 'Arun Kumar',
+        GuestNumber: '2',
+        PhoneNumber: '',
+        email: 'arunk9805@gmail.com',
+        time: '05:14 PM',
+        date: "2024-03-25T00:00:00.000Z",
+      }
+      const convertedDate =  formatDate(saveGuestData.date);
+    res.render(__dirname + "/views/regSuccess.ejs", {saveGuestData, convertedDate});
 });
 
 app.post("/regSuccess", async (req, res) => {
     try {
         const guestData = new GuestData(req.body);
         const saveGuestData = await guestData.save();
-        console.log("Reservation Successful: ", saveGuestData);
-        
-        res.render(__dirname + "/views/regSuccess.ejs", {saveGuestData});
+        const convertedDate =  formatDate(saveGuestData.date);
+        console.log("Reservation Successful: ", saveGuestData, convertedDate);     
+        res.render(__dirname + "/views/regSuccess.ejs", {saveGuestData, convertedDate});
     } catch (error) {
         console.log("Error while saving data: ", error);
         res.status(500).send("Server-side ERROR!!\n Error while saving data!")
